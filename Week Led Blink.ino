a0307bc67c5f2ebbb812627902d8f46dae198634
tanish-uppal <uppaltanish00@gmail.com>
2026-07-07 04:08:51 +0530

    Rename file to Week Led Blink.ino

---
+++ Week Led Blink.ino
@@ -0,0 +1,19 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13,HIGH);
+  delay(300);
+  digitalWrite(12,HIGH);
+  delay(500); 
+  digitalWrite(13,LOW);
+  delay(100);
+  digitalWrite(12,LOW);
+  delay(500); 
+}
